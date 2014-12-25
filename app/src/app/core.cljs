223f30220592415db50879e3030bdc0983e4a848
NikolaMandic <mn080202@gmail.com>
2014-12-25 04:50:15 +0100

    added servant for communicating with miner

--- app/src/app/core.cljs
+++ app/src/app/core.cljs
@@ -3,4 +3,6 @@
 (enable-console-print!)
 (. js/console (log (THREE/Scene. )))
 (println "Hello wor 32 d rdaldad!")
-(def myWorker (js/Worker. "wrkr.js"))
+(def myWorker (js/Worker. "hamiyoca/miner.js"))
+(def ^:dynamic o (js-obj "foo" 1 "bar" 2))
+(. myWorker (postMessage o))
