323d351c7ef17624ebbd7930c8b6168972e4dd2e
NikolaMandic <user@linux-olao.site>
2014-12-25 04:50:15 +0100

    added servant for communicating with miner

--- app/project.clj
+++ app/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [servant "0.1.3"]
+                 ]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
