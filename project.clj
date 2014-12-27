83ecaeafc0ab9551b20400c9a9b6260e63707550
NikolaMandic <mn080202@gmail.com>
2014-12-27 03:15:55 +0100

    just pictures this time

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]]
-
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]]}}
+  
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
   :source-paths ["src"]
