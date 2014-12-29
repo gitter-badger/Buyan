dc6a676e99b38ac0e64a4c9746c856cb6de49a32
NikolaMandic <mn080202@gmail.com>
2014-12-29 04:07:22 +0100

    crypto api gets invoked

--- app/src/app/blockchain.cljs
+++ app/src/app/blockchain.cljs
@@ -2,16 +2,26 @@
 (:require
     [app.logger :as l]))
 (enable-console-print!)
+(def memPool (array))
+(defn addTransactionToMemPool [x] 
+(.push memPool x)
+(l/og :blockchain "new memPool after adding " memPool))
+(defn removeTransactionFromMemPool [x] (.splice memPool (.indexOf memPool x))
+
+ (l/og :blockchain "new memPool after remove " memPool)
+)
 (defn makeBlock [] (js-obj "" 1))
 (defn makeTransaction [] (js-obj "" 1))
 (defn addTransactionToBlock [] (js-obj "" 1))
 (defn encode [x] (.encode (js/TextEncoder. "utf-8") x))
+(defn shaCallb [digest]
+    (l/og :blockchain "%s" "blockchain")
+  )
 (defn sha256 [x] 
     (l/og :blockchain "%s" "sha256")
- (.then (js/crypto.subtle.digest (js-obj name "SHA-256") (encode x) )
-  (fn [digest]
-    (l/og :blockchain "blockchain")
-  )
+    (l/og :blockchain "%s" (encode "sha256"))
+ (.then (js/crypto.subtle.digest (js-obj "name" "SHA-256") (encode x) )
+  shaCallb
  )
 )
 (defn merkleRoot [transactions] ())
