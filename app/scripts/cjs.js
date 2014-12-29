d5dabe754a3cab3098cb99da9f2ea55e83603047
NikolaMandic <mn080202@gmail.com>
2014-12-29 06:27:01 +0100

    messing with arraybuffers in clojurescript is ticky

--- app/scripts/cjs.js
+++ app/scripts/cjs.js
@@ -1,3 +1,16 @@
-function cls(digest){
-    console.log("callback");
+function arrayBToString(digest){
+    	function _arrayBufferToBase64( buffer ) {
+						var ret = "",
+						  i = 0,
+						  len = buffer.length;
+						while (i < len) {
+							  var a = buffer[i];
+								  var h = (a < 10 ? "0" : "") + a.toString(16);
+									  ret += h;
+										  i++;
+						}
+						return ret;
+					}
+					var str = _arrayBufferToBase64(new Uint8Array(digest));
+					return str
 }
