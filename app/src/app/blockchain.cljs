d581fece10d5a0eea33ba4c1693611af5265a58a
NikolaMandic <mn080202@gmail.com>
2014-12-30 14:44:59 +0100

    fake merkle root; it is chain hash not tree hash

--- app/src/app/blockchain.cljs
+++ app/src/app/blockchain.cljs
@@ -33,6 +33,12 @@
    (go
    (>! app.main.cryptoCh h )
   )))
+  (def shaC (chan))
+(defn resultToCh [ chan digest]
+  (def m (arraybtostring digest))
+  (l/og :blockchain "to channel: %s" m)
+ (go (>! chan m))
+)
 (defn sha256 [x] 
     ;(l/og :blockchain "%s" "sha256")
     ;(l/og :blockchain "%s" (encode "sha256"))
@@ -40,4 +46,47 @@
   shaCallb
  )
 )
-(defn merkleRoot [transactions] ())
+(defn sha256c [c x] 
+    ;(l/og :blockchain "%s" "sha256")
+    ;(l/og :blockchain "%s" (encode "sha256"))
+ (.then (js/crypto.subtle.digest (js-obj "name" "SHA-256") (encode x) )
+  c
+ )
+)
+(defn popA [a]
+ (.splice a 0 1)
+)
+    (defn log2 [n]
+    (/ (Math/log n) (Math/log 2)))
+(defn merkleRoot [transactions]  
+  ;note this is fake merkle root but does the trick 
+
+  (def originl (.-length transactions))
+  (def tx (.shift transactions))
+  (def next (.shift transactions))
+  (sha256c (partial resultToCh shaC) (+ tx next))
+  (loop [cnt (.-length transactions)   next (.shift  transactions ) txs   transactions ]
+  (l/og :blockchain "count  %s" cnt)
+  (go
+    (def tx (<! shaC))
+    )
+  (sha256c (partial resultToCh shaC) (+ tx next))
+  (if (== 0 cnt)
+  0
+  (recur (.-length txs)  (.shift txs)   txs )))
+
+;  (loop [cnt (/ originl 2)   ]
+;  (l/og :blockchain "countt %s " cnt )
+;  (go
+;    (def tx (<! shaC))
+;    (def next (<! shaC))
+;  );
+
+;  (sha256c (partial resultToCh shaC) (+ tx next))
+;  (if (== 1 cnt)
+;  0
+;  (recur (/ cnt 2) )))
+
+
+)
+; 2^
