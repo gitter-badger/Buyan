83e670b4add483f275337e1e4cc7cc771f251771
NikolaMandic <mn080202@gmail.com>
2014-12-30 22:10:56 +0100

    starting to save stuff in pouchdb

--- app/src/app/logger.cljs
+++ app/src/app/logger.cljs
@@ -1,6 +1,6 @@
 (ns app.logger)
 (enable-console-print!)
-(def tagsOn [:mloop :blockchain])
+(def tagsOn [:dbname :db])
 (defn og [type format data] 
   (if (or (some  #{type} tagsOn) (some #{:all} tagsOn))
   (.log js/console format data )
