f3181f9947213193b29e82486bc351e2b4d11837
NikolaMandic <mn080202@gmail.com>
2014-12-31 08:32:03 +0100

    plowing trough blockchain generation

--- app/src/app/logger.cljs
+++ app/src/app/logger.cljs
@@ -1,6 +1,6 @@
 (ns app.logger)
 (enable-console-print!)
-(def tagsOn [:dbname :db])
+(def tagsOn [:dbname :db :all])
 (defn og [type format data] 
   (if (or (some  #{type} tagsOn) (some #{:all} tagsOn))
   (.log js/console format data )
