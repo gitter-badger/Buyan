879bb99dd78680065e4a2e2103b70817db1b5cdc
NikolaMandic <mn080202@gmail.com>
2015-01-22 22:20:28 +0100

    formating

--- app/src/app/intercomMake.cljs
+++ app/src/app/intercomMake.cljs
@@ -1,1 +1,15 @@
-(ns intercomMake)
+(ns intercomMake
+  (:require
+    [app.logger :as l]
+    [app.database :as db]
+
+    [app.blockchain :as blockchain]
+    [cljs.core.async :refer [chan close! timeout put!]]
+
+    )
+
+  (:require-macros [cljs.core.async.macros :as m :refer [go]])
+  )
+(defn makeInv [typ message]
+
+      )
--- app/src/app/intercomTake.cljs
+++ app/src/app/intercomTake.cljs
@@ -1,1 +1,11 @@
-s (ns intercomTake)
+(ns intercomTake
+  (:require
+    [app.logger :as l]
+    [app.database :as db]
+
+    [app.blockchain :as blockchain]
+    [cljs.core.async :refer [chan close! timeout put!]]
+
+    )
+  (:require-macros [cljs.core.async.macros :as m :refer [go]])
+  )
