ecd75f336e3da82fdc285c9ccb8d4fb95d03e666
NikolaMandic <mn080202@gmail.com>
2015-01-23 22:59:36 +0100

    refactoring

--- app/src/app/blockchain.cljs
+++ app/src/app/blockchain.cljs
@@ -36,10 +36,6 @@
 
 (defn makeTransaction [] (js-obj "" 1))
 (defn addTransactionToBlock [] (js-obj "" 1))
-(defn encode [x] (.encode (js/TextEncoder. "utf-8") x))
-(defn arraybtostring [buff]
-      (js/arrayBToString buff)
-      )
 ;get previous block
 (defn prevblk [blockk]
       (l/og :prevblk "about to get prev blk " blockk)
@@ -184,87 +180,14 @@
           )
         )
       )
-(defn shaCallb [digest] (do
-                          (l/og :blockchain "%s" "about to do hash2")
-                          (def h (arraybtostring digest))
-                          (l/og :blockchain h)
-                          (addTransactionToMemPool h)
-                          (go
-                            (>! app.main.cryptoCh h)
-                            )))
-(def shaC (chan))
-(defn resultToCh [chan digest]
-      (def m (arraybtostring digest))
-      (l/og :blockchain "to channel: %s" m)
-      (go (>! chan m))
-      )
-(defn sha256 [x]
-      ;(l/og :blockchain "%s" "sha256")
-      ;(l/og :blockchain "%s" (encode "sha256"))
-      (.then (js/crypto.subtle.digest (js-obj "name" "SHA-256") (encode x))
-             shaCallb
-             )
-      )
-(defn sha256c [c x]
-      ;(l/og :blockchain "%s" "sha256")
-      ;(l/og :blockchain "%s" (encode "sha256"))
-      (.then (js/crypto.subtle.digest (js-obj "name" "SHA-256") (encode x))
-             c
-             )
-      )
-(defn s256 [k]
-      (l/og :blockchain "about to sha256 " k)
-      (go
-        (let [c (chan)]
-             (.then (js/crypto.subtle.digest (js-obj "name" "SHA-256") (encode k)) #(put! c %))
 
 
-             (def r (arraybtostring (<! c)))
-             (l/og :blockchain (+ "got from sha256 " k) r)
-             r
-             )
-        )
-      )
 (defn popA [a]
       (.splice a 0 1)
       )
 (defn log2 [n]
       (/ (Math/log n) (Math/log 2)))
 
-;note this is fake merkle root but does the trick it hashes chain of transactions
-;for now it will be here due to simplicity and bcz I could not find math functions in clojurescript easily
-;merkleRoot is more efficient at getting any transaction in block because you need to supply only path to the root to verify
-;while here it varies depending whether transaction searched is last one or first one
-;will save mempool here so we can retrieve them later when merkle root is ready and when block structure is to be made
-
-(defn merkleRoot [transactions]
-
-
-      (db/update "txs" #(do transactions))
-      (go
-        (def originl (.-length transactions))
-        (def tx (.shift transactions))
-        (def next (.shift transactions))
-        (sha256c (partial resultToCh shaC) (+ tx next))
-        (loop [next (.shift transactions) cnt (.-length transactions) txs transactions]
-              (l/og :blockchain "count  %s" cnt)
-
-              (def tx (<! shaC))
-              (sha256c (partial resultToCh shaC) (+ tx next))
-              (if (== 0 cnt)
-                (go
-
-                  )
-                (recur (.shift txs) (.-length txs) txs)))
-
-        (def fromC (<! shaC))
-        (l/og :blockchain "from ch " fromC)
-        (set! (.-type fromC) "fmr")
-
-        (>! app.main.cryptoCh (js-obj "value" fromC "type" "fmr"))
-
-        )
-      )
 ;(def lastt 1)
 ;(l/og :blockchain "got last" lastt)
 ; lastt
