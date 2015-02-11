d5465631c9d400828122c8d54a1c297456ecbac1
user <nikola@datamonk.com>
2015-02-11 20:05:12 +0100

    save

--- app/test/test.cljs
+++ app/test/test.cljs
@@ -7,7 +7,86 @@
                        :as tt
                        :refer (is deftest with-test run-tests testing test-var)] )
    )
+(deftest initdbase
+  ;check for root in database
+  ;;clean db
+  ;;initdb
+  ;;get root
+  ;;check if root
+  )
+(deftest makeBlockHeader
+  ;;make header
+  ;;check if it returns properly
+  )
+(deftest makeBlockBlock
+  ;;make header
+  ;;make block
+  ;;check if block returned properly
+  )
+(deftest saveblock
+  ;check for root in database
+  ;;clean db
+  ;;initdb
+  ;;make block
+  ;;save block
+  ;;check block
+  )
+(deftest blockheight
+  ;;add block 
+  ;;check height
+  )
+(deftest sha256
+  ;;hash something
+  ;;check hash
+  )
+(deftest transaction-making
+  ;;feed input 
+  ;;make transaction
+  )
+(deftest merkleroot
+  ;;make transactions
+  ;;get fake merkleroot
+  ;;check hash
+  )
+(deftest transaction-in-block
+  ;;make transactions
+  ;;add them to block
+  ;;check block
+  )
+(deftest mineblock
+  ;;feed block into miner
+  ;;check mined
+  )
+(deftest broadcastblock
+  ;;
+  ;;broadcast send
+  )
+(deftest blocksyncEmptyBase
+  ;;mock empty dbase
+  ;;block recieved when database empty
+  ;;send get them from root
+  )
+(deftest getblocks
+  ;;get blockchain history *2 increments etc
+  )
+(deftest blocksyncLowerBase
+  ;; mock database of lower height
+  ;; block recieved when database is of lower height
+  ;; send history
+  )
+(deftest sendblocks
+  ;; send from last known block
+  )
+(deftest blocksyncHigherBaseHasBlockInDB
 
+  
+  )
+(deftest blocksyncHigherBaseDoesNotHaveBlockInDB
+  
+  )
+(deftest blocksyncForkBase
+  
+  )
  (deftest somewhat-less-wat
           ;(.log js/console js/PouchDB)
           (is (= "{}[]" (+ {} []))))
