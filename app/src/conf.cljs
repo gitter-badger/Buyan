5b6e2ed4ac56b1daed5a684204cf1a93352ce1e4
NikolaMandic <mn080202@gmail.com>
2015-03-07 19:42:16 +0100

    a

---
+++ app/src/conf.cljs
@@ -0,0 +1,13 @@
+(ns conf)
+(def signaling2 "buyan-nikolamandic.rhcloud.com")
+(def signaling1 "localhost")
+
+
+
+
+(def signaling signaling2)
+(def sport1 "8000")
+(def sport2 "80")
+(def sport sport2)
+(def signalingd (str signaling ":" sport))
+(def interval 1000)
