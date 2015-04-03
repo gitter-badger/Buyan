1a8ebf473ddaed560ca019bf87c298a7f4fbb20d
NikolaMandic <mn080202@gmail.com>
2015-04-04 00:54:23 +0200

    prototyping ui

---
+++ bapp/app/views/sidemenu.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+ didInsertElement : function(){
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, function(){
+    	// perform your jQuery logic here
+    	   $('.accordion')
+	      .accordion({
+	        selector: {
+	          trigger: '.title .icon'
+	        }
+	      })
+	    ;
+    });
+  }
+});
