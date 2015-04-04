5b5680d1148a11c414b66e5cbc724c34134f860e
NikolaMandic <mn080202@gmail.com>
2015-04-04 20:06:34 +0200

    prototyping ui

--- bapp/config/environment.js
+++ bapp/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
 
-    ENV.baseURL= '/Buyan/bapp/dist/';
+    ENV.baseURL= '/Buyan/';
     ENV.fingerprint= {
       enabled: false
     }
