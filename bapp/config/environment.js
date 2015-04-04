7286f2e3b1a883b5f73d2a1f7de716c1164dd76f
NikolaMandic <mn080202@gmail.com>
2015-04-04 18:55:43 +0200

    prototyping ui

--- bapp/config/environment.js
+++ bapp/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
 
-    ENV.baseURL= undefined;
+    ENV.baseURL= '/Buyan/bapp/dist/';
   }
 ENV.contentSecurityPolicy = {
   'default-src': "'none'",
